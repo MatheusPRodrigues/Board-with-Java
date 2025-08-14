9b8ceb6cbf6ea07de5f5c852361188617e88c5df
MatheusPRodrigues <matherodrigues17@gmail.com>
2025-08-14 09:21:49 -0300

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.persistence.migration.MigrationStrategy;
+
+import java.sql.SQLException;
+
+import static org.example.persistence.config.ConnectionConfig.getConnection;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        try(var connection = getConnection()) {
+            new MigrationStrategy(connection).executeMigration();
+        }
+    }
+}
